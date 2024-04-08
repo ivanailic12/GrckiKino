27061d0e87a52a7d8888195d6f26134523bd3a4c
Ivana Ilic <ivanailic.cs@gmail.com>
2024-04-08 21:48:26 +0200

    Initial Commit

---
+++ GrckiKino/GrckiKinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrckiKinoApp.swift
+//  GrckiKino
+//
+//  Created by Ivana Crystal Pigeon on 8.4.24..
+//
+
+import SwiftUI
+
+@main
+struct GrckiKinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
